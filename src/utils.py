9f44c1bc5e018dd0db7e64b4cb11643e7b8fa9e0
Gabriele <gabrieletuozzo@gmail.com>
2024-12-22 15:20:45 +0000

    fix: removed the SSL verification

--- src/utils.py
+++ src/utils.py
@@ -277,7 +277,7 @@ def checkAvailabilityResource(url):
         if response.status_code < 400:   #IF FAILS WITH A HEAD REQUEST, WE TEST WITH A GET (HEAD MAY NOT BE SUPPORTED)
             return True
         else:
-            response = requests.head(url,timeout=180,allow_redirects=True, verify=False)
+            response = requests.head(url,timeout=180,allow_redirects=True, verify=False, verify=False)
             if response.status_code < 400:
                 return True
             else:
@@ -287,7 +287,7 @@ def checkAvailabilityResource(url):
                     if response.status_code < 400:
                         return True
                     else:
-                         response = requests.head(newUrl,timeout=180,allow_redirects=True, verify=False)
+                         response = requests.head(newUrl,timeout=180,allow_redirects=True, verify=False, verify=False)
                          if response.status_code < 400:
                              return True
                          else:
