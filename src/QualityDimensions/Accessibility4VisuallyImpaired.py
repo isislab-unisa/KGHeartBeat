1054adb4ff00fa3a66b865793eaae9f600cc349f
Gabriele <gabrieletuozzo@gmail.com>
2025-11-11 02:39:49 +0000

    fix: standardize timeout parameter naming in run_with_timeout calls for image and video metadata functions

--- src/QualityDimensions/Accessibility4VisuallyImpaired.py
+++ src/QualityDimensions/Accessibility4VisuallyImpaired.py
@@ -33,7 +33,7 @@ class Accessibility4VisuallyImpaired:
                 return 0, f"Error fetching images: {images}"
     
     def audio_meta(self, sparql_endpoint, void_file_url, resources):
-        return utils.run_with_timeout(utils.check_metadata_media_type,args=(sparql_endpoint, void_file_url, resources, "audio/",), timeout_duration=15)
+        return utils.run_with_timeout(utils.check_metadata_media_type,args=(sparql_endpoint, void_file_url, resources, "audio/",), timeout=15)
     
     def audio(self, sparql_endpoint):
         if not utils.is_url(sparql_endpoint):
